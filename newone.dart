9aa025dcb88653d0f9fc008b1a11934176fa3744
dhirajchaurasiya10 <106879418+dhirajchaurasiya10@users.noreply.github.com>
2023-01-10 16:14:48 +0545

    Update newone.dart

--- newone.dart
+++ newone.dart
@@ -19,7 +19,7 @@ void main() {
   List number = [1, 2, 3, "Dhiraj"];
   print(number);
 
-  List<String> str = ["dhiraj", "robert", "govind", "govind", "ram"];
+  List<String> str = ["dhiraj", "robert", "govind", "govind", "ram"];             //example of list!
   print(str[4]);
   List<int> age = [3, 23, 37, 20];
   print(age[2]);
@@ -32,7 +32,7 @@ void main() {
     "address": 456,
   };
   print(intro);
-  Map<String, String> newone = {
+  Map<String, String> newone = {                                                //example of map!
     "name": "Dhiraj",
     "address": "Butwal",
   };
@@ -42,16 +42,16 @@ void main() {
   Map question = {
     // "qn1": "Where was Buddha born?",
     "answers": ["Nepal", "India", "Pakistan", "Bhutan"],
-    // "correctopt": 0,
+    // "correctopt": 0,                                   //These lines are commented cause a map takes single type of data(either int only or string or list)
     // "score": 5,
   };
   print(question["qn1"]);
   print(question["answers"]);
-  question.forEach((key, value) {
+  question.forEach((key, value) {                     //for each is a loop that helps to format (or let's say filter in local language) for each key and value!
     print(value);
     print("${key}=${value}");
   });
-  List one = ["hari", "ram", "shyam"];
+  List one = ["hari", "ram", "shyam"];               //This is spread, it makes to substitute one list to another.
   List two = ["gita", ...one, "sita"];
   print(two);
 }
