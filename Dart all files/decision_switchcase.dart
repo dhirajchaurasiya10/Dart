966232baa64ed531ef31eeadb9c48ff61a49d2af
Dhiraj <starkjon224@gmail.com>
2023-10-19 14:58:49 +0545

    new

---
+++ Dart all files/decision_switchcase.dart
@@ -0,0 +1,20 @@
+void main() {
+  var atebreakfast = 'Icecream';
+  switch (atebreakfast) {
+    case "Eggs":
+      {
+        print(":)");
+      }
+      break;
+    case "Milk":
+      {
+        print(":(");
+      }
+      break;
+    default:
+      {
+        print(":(");
+      }
+      break;
+  }
+}
