81c93a91b4be0c2bd5c17ad36dd8cad8eaf4a91f
Dhiraj <starkjon224@gmail.com>
2023-10-20 11:17:43 +0545

    updated

---
+++ Dart all files/5loops.dart
@@ -0,0 +1,18 @@
+void main() {
+  var x = 0;
+  while (x < 5) {
+    print(x);
+    x = x + 1;
+  }
+
+  var lis1 = ['a', 'b', 'c', 'd'];
+  for (var character in lis1) {
+    print(character);
+  }
+
+  var n = 1;
+  do {
+    print(n);
+    n += 1;
+  } while (n <= 5);
+}
